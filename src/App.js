96760e3f533b2490525c11215d2e359300add2d2
Subham Tandukar <subhamtndkr9@gmail.com>
2022-07-13 19:11:04 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 import "./animate.css";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Redirect } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Homepage from "./components/Homepage";
-import ErrorPage from "./components/ErrorPage";
 
 const App = () => {
   return (
@@ -16,7 +15,7 @@ const App = () => {
         <Route path="/" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<Homepage />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Redirect to="/" />
       </Routes>
     </>
   );
